92c4d521abb8ceb5b1dba132a176cbb39b7ac677
Pedro Savelis <pedro@57blocks.com>
2026-03-15 22:20:20 -0300

    feat: add no-copy repository constructor

--- pkg/mongodb/constructors.go
+++ pkg/mongodb/constructors.go
@@ -25,3 +25,22 @@ func NewMongoDBRepository[T shared.Entity](mongoClient *mongo.Client, dbName str
 		QueryableFields:   make(map[string]bool),
 	}
 }
+
+// NewMongoDBRepositoryForType creates a new generic MongoDB repository instance without requiring a value copy of T.
+func NewMongoDBRepositoryForType[T shared.Entity](mongoClient *mongo.Client, dbName string, collectionName string, entityName string) *MongoDBRepository[T] {
+	entityType := reflect.TypeFor[T]()
+	if entityType.Kind() == reflect.Ptr {
+		entityType = entityType.Elem()
+	}
+
+	return &MongoDBRepository[T]{
+		mongoClient:       mongoClient,
+		dbName:            dbName,
+		mappingCache:      make(map[string]CacheItem),
+		entityModel:       entityType,
+		collectionName:    collectionName,
+		entityName:        entityName,
+		BsonFieldMappings: make(map[string]string),
+		QueryableFields:   make(map[string]bool),
+	}
+}
